ec0a9d3a038fbea8d8ecf0df01dc12e9cafeb466
Rebecca Mark <rebecca.lmark@gmail.com>
2019-10-17 22:20:45 -0700

    implements mongean shuffle and adds tests

--- src/main/scala/stanza/rotation/MongeanShuffle.scala
+++ src/main/scala/stanza/rotation/MongeanShuffle.scala
@@ -1,33 +1,25 @@
 package stanza.rotation
 
 class MongeanShuffle extends NTinaRotation {
-  def shuffle[A](list: List[A]) : List[A] = {
-    list.zipWithIndex.foldLeft(List.empty[A]){ case (acc, (word, index)) =>
+  def shuffle[T](list: List[T]) : List[T] = {
+    list.zipWithIndex.foldLeft(List.empty[T]){ case (acc, (word, index)) =>
       if (index % 2 == 0) acc :+ word
       else word +: acc
     }
   }
 
-  def nextStanza[A](list: List[A]) = {
-    // 1.) Take your list and perform a Mongean shuffle
-    val originalList = list
-    val shuffledList = shuffle(list)
+  def shuffleReverse[T](list: List[T]): List[T] = {
+    def loop(currentL: List[T], acc: List[T]): List[T] = {
+      currentL match {
+        case Nil => acc
+        case h::tail => loop(tail.reverse,  acc :+ h)
+      }
+    }
+    loop(list, Nil)
+  }
 
-    println(s"original list: ${originalList.mkString(", ")}")
-    println(s"shuffled List: ${shuffledList.mkString(", ")}")
-    // 2. Flip it left to right
-    val flippedOriginal = originalList.reverse
-    val flippedShuffled = shuffledList.reverse
-
-    println(s"reversed original: ${flippedOriginal.mkString(", ")}")
-    println(s"reversed shuffled: ${flippedShuffled.mkString(", ")}")
-    // 3. Flip these two vertically, so the next and original swap places
-    val swappedOriginal = flippedShuffled
-    val swappedNext = flippedOriginal
-
-    println(s"new first stanza: ${swappedOriginal.mkString(", ")}")
-    println(s"new second stanza: ${swappedNext.mkString(", ")}")
-
-    swappedOriginal
+  def nextStanza[T](list: List[T]): List[T] = {
+    val flippedOriginal = list.reverse
+    shuffleReverse(flippedOriginal)
   }
 }
--- src/test/scala/stanza/rotation/MongeanShuffleTest.scala
+++ src/test/scala/stanza/rotation/MongeanShuffleTest.scala
@@ -1,19 +1,5 @@
 package stanza.rotation
 
-import org.scalatest._
-
-class MongeanShuffleTest extends WordSpec with Matchers {
-  val monge = new MongeanShuffle
-
-  "Mongean Shuffle" should {
-    "create a valid ordering for list of even length" in {
-      val list = List(1,2,3,4,5,6)
-      monge.shuffle(list) shouldBe List(6,4,2,1,3,5)
-    }
-    "create a valid ordering for a list of odd length" in {
-      val list = List(1,2,3,4,5)
-      monge.shuffle(list) shouldBe List(4,2,1,3,5)
-    }
-  }
-
+class MongeanShuffleTest extends NTinaRotationTestHarness[MongeanShuffle]{
+  override val rotationImpl: MongeanShuffle = new MongeanShuffle()
 }
