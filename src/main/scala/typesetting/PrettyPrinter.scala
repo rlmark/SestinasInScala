fcafbba1949ad91011f85798dac898b4fd82b28f
Rebecca Mark <rebecca.lmark@gmail.com>
2019-10-21 21:29:12 -0700

    adds list,vector,option print impl

--- src/main/scala/typesetting/PrettyPrinter.scala
+++ src/main/scala/typesetting/PrettyPrinter.scala
@@ -23,8 +23,20 @@ object PrettyPrinterImpl {
   implicit def stanzaPrinter(implicit pp: PrettyPrinter[Line]): PrettyPrinter[Stanza] = new PrettyPrinter[Stanza] {
     override def print(a: => Stanza): String = a match {
       case Sestet(l1, l2, l3, l4, l5, l6) => List(pp.print(l1), pp.print(l2), pp.print(l3), pp.print(l4), pp.print(l5), pp.print(l6))
-        .mkString("\n") + "\n"
+        .mkString("\n")
       case Envoi(l1, l2, l3) => List(pp.print(l1), pp.print(l2), pp.print(l3)).mkString("\n")
     }
+  }
+
+  implicit def listPrinter[A: PrettyPrinter]: PrettyPrinter[List[A]] = new PrettyPrinter[List[A]] {
+    override def print(as: => List[A]): String = as.foldLeft("") { case (acc: String, a: A) => acc + "\n" + PrettyPrinter[A].print(a) }
+  }
+
+  implicit def vectorPrinter[A: PrettyPrinter]: PrettyPrinter[Vector[A]] = new PrettyPrinter[Vector[A]] {
+    override def print(as: => Vector[A]): String = as.foldLeft("") { case (acc: String, a: A) => acc + "\n" + PrettyPrinter[A].print(a) }
+  }
+
+  implicit def optionPrinter[A: PrettyPrinter]: PrettyPrinter[Option[A]] = new PrettyPrinter[Option[A]] {
+    override def print(as: => Option[A]): String = as.fold("")((a: A) => PrettyPrinter[A].print(a))
   }
 }
